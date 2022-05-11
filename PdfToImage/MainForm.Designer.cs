a617d01e2e54ce1aaadf5ff3130fb1cfbfcd1b59
Kuang Qidong <kuangqidong@126.com>
2022-05-11 15:01:33 +0800

    Initial upload

---
+++ PdfToImage/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace PdfToImage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxImageSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "图片大小:";
+            // 
+            // txtBoxImageSize
+            // 
+            this.txtBoxImageSize.Location = new System.Drawing.Point(101, 20);
+            this.txtBoxImageSize.Name = "txtBoxImageSize";
+            this.txtBoxImageSize.Size = new System.Drawing.Size(133, 21);
+            this.txtBoxImageSize.TabIndex = 1;
+            this.txtBoxImageSize.Text = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MB";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(44, 57);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(213, 38);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "开始转换";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 122);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxImageSize);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF转JPG v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxImageSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ PdfToImage/MainForm.cs
@@ -0,0 +1,343 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PdfToImage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertPdfToImage(string sourceFile, string destiPath, double fileSizeInMb)
+        {
+            Image combinedImage = null;
+            var fileList = new List<string>();
+            var name = Path.GetFileNameWithoutExtension(sourceFile);
+            var combinedFile = Path.Combine(Path.GetDirectoryName(destiPath), name + ".png");
+
+            try
+            {
+                using (var document = PdfDocument.Load(sourceFile))
+                {
+                    var pageCount = document.PageCount;
+                    for (int i = 0; i < pageCount; i++)
+                    {
+                        var dpi = 300;
+
+                        using (var image = document.Render(i, dpi, dpi, PdfRenderFlags.CorrectFromDpi))
+                        {
+                            var encoder = ImageCodecInfo.GetImageEncoders()
+                                .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+                            var encParams = new EncoderParameters(1);
+                            encParams.Param[0] = new EncoderParameter(
+                                System.Drawing.Imaging.Encoder.Quality, 100L);
+                            var filePath = Path.Combine(Path.GetDirectoryName(destiPath), $"{name}_{i}.png");
+                            fileList.Add(filePath);
+
+                            image.Save(filePath, encoder, encParams);
+                            image.Dispose();
+                        }
+                    }
+                }
+
+                CombineImages(fileList.ToArray(), combinedFile, ImageMergeOrientation.Vertical);
+                combinedImage = Image.FromFile(combinedFile);
+                var zippedImage = ZipImage(combinedImage, ImageFormat.Jpeg, Convert.ToInt64(fileSizeInMb * 1024), new FileInfo(combinedFile).Length);
+                zippedImage.Save(destiPath);
+                zippedImage.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                fileList.ForEach(x => File.Delete(x));
+                combinedImage?.Dispose();
+                File.Delete(combinedFile);
+            }
+
+        }
+
+        enum ImageMergeOrientation
+        {
+            Horizontal,
+            Vertical
+        }
+
+        private void CombineImages(string[] files, string toPath, ImageMergeOrientation mergeType = ImageMergeOrientation.Vertical)
+        {
+            //change the location to store the final image.
+            var finalImage = toPath;
+            var imgs = files.Select(f => Image.FromFile(f)).ToList();
+
+            var finalWidth = mergeType == ImageMergeOrientation.Horizontal ?
+                imgs.Sum(img => img.Width) :
+                imgs.Max(img => img.Width);
+
+            var finalHeight = mergeType == ImageMergeOrientation.Vertical ?
+                imgs.Sum(img => img.Height) :
+                imgs.Max(img => img.Height);
+
+            var finalImg = new Bitmap(finalWidth, finalHeight);
+            Graphics g = Graphics.FromImage(finalImg);
+            g.Clear(SystemColors.AppWorkspace);
+
+            var width = finalWidth;
+            var height = finalHeight;
+            var nIndex = 0;
+            foreach (var img in imgs)
+            {
+                //Image img = Image.FromFile(file);
+                if (nIndex == 0)
+                {
+                    g.DrawImage(img, 0, 0, img.Width, img.Height);
+                    nIndex++;
+                    width = img.Width;
+                    height = img.Height;
+                }
+                else
+                {
+                    switch (mergeType)
+                    {
+                        case ImageMergeOrientation.Horizontal:
+                            g.DrawImage(img, width, 0, img.Width, img.Height);
+                            width += img.Width;
+                            break;
+                        case ImageMergeOrientation.Vertical:
+                            g.DrawImage(img, 0, height, img.Width, img.Height);
+                            height += img.Height;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException("mergeType");
+                    }
+                }
+                img.Dispose();
+            }
+            imgs.Clear(); 
+            g.Dispose();
+            finalImg.Save(finalImage, System.Drawing.Imaging.ImageFormat.Png);
+            finalImg.Dispose();
+        }
+
+        /// <summary>
+        /// 压缩图片
+        /// </summary>
+        /// <param name="img">图片</param>
+        /// <param name="format">图片格式</param>
+        /// <param name="targetLen">压缩后大小</param>
+        /// <param name="srcLen">原始大小</param>
+        /// <returns>压缩后的图片</returns>
+        public Image ZipImage(Image img, ImageFormat format, long targetLen, long srcLen = 0)
+        {
+            //设置大小偏差幅度 10kb
+            const long nearlyLen = 10240;
+            //内存流  如果参数中原图大小没有传递 则使用内存流读取
+            var ms = new MemoryStream();
+            if (0 == srcLen)
+            {
+                img.Save(ms, format);
+                srcLen = ms.Length;
+            }
+
+            //单位 由Kb转为byte 若目标大小高于原图大小，则满足条件退出
+            targetLen *= 1024;
+            if (targetLen > srcLen)
+            {
+                ms.SetLength(0);
+                ms.Position = 0;
+                img.Save(ms, format);
+                img = Image.FromStream(ms);
+                return img;
+            }
+
+            //获取目标大小最低值
+            var exitLen = targetLen - nearlyLen;
+
+            //初始化质量压缩参数 图像 内存流等
+            var quality = (long)Math.Floor(100.00 * targetLen / srcLen);
+            var parms = new EncoderParameters(1);
+
+            //获取编码器信息
+            ImageCodecInfo formatInfo = null;
+            var encoders = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo icf in encoders)
+            {
+                if (icf.FormatID == format.Guid)
+                {
+                    formatInfo = icf;
+                    break;
+                }
+            }
+
+            //使用二分法进行查找 最接近的质量参数
+            long startQuality = quality;
+            long endQuality = 100;
+            quality = (startQuality + endQuality) / 2;
+
+            while (true)
+            {
+                //设置质量
+                parms.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+
+                //清空内存流 然后保存图片
+                ms.SetLength(0);
+                ms.Position = 0;
+                img.Save(ms, formatInfo, parms);
+
+                //若压缩后大小低于目标大小，则满足条件退出
+                if (ms.Length >= exitLen && ms.Length <= targetLen)
+                {
+                    break;
+                }
+                else if (startQuality >= endQuality) //区间相等无需再次计算
+                {
+                    break;
+                }
+                else if (ms.Length < exitLen) //压缩过小,起始质量右移
+                {
+                    startQuality = quality;
+                }
+                else //压缩过大 终止质量左移
+                {
+                    endQuality = quality;
+                }
+
+                //重新设置质量参数 如果计算出来的质量没有发生变化，则终止查找。这样是为了避免重复计算情况{start:16,end:18} 和 {start:16,endQuality:17}
+                var newQuality = (startQuality + endQuality) / 2;
+                if (newQuality == quality)
+                {
+                    break;
+                }
+                quality = newQuality;
+                //Console.WriteLine("start:{0} end:{1} current:{2}", startQuality, endQuality, quality);
+            }
+            img = Image.FromStream(ms);
+            return img;
+        }
+
+        /// <summary>
+        ///获取图片格式
+        /// </summary>
+        /// <param name="img">图片</param>
+        /// <returns>默认返回JPEG</returns>
+        public ImageFormat GetImageFormat(Image img)
+        {
+            if (img.RawFormat.Equals(ImageFormat.Jpeg))
+            {
+                return ImageFormat.Jpeg;
+            }
+            if (img.RawFormat.Equals(ImageFormat.Gif))
+            {
+                return ImageFormat.Gif;
+            }
+            if (img.RawFormat.Equals(ImageFormat.Png))
+            {
+                return ImageFormat.Png;
+            }
+            if (img.RawFormat.Equals(ImageFormat.Bmp))
+            {
+                return ImageFormat.Bmp;
+            }
+            return ImageFormat.Jpeg;//根据实际情况选择返回指定格式还是null
+        }
+        /// <summary>
+        /// 不管多大的图片都能在指定大小picturebox控件中显示
+        /// </summary>
+        /// <param name="bitmap">图片</param>
+        /// <param name="destHeight">picturebox控件高</param>
+        /// <param name="destWidth">picturebox控件宽</param>
+        /// <returns></returns>
+        private Image ZoomImage(Image bitmap, int destHeight, int destWidth)
+        {
+            try
+            {
+                System.Drawing.Image sourImage = bitmap;
+                int width = 0, height = 0;
+                //按比例缩放             
+                int sourWidth = sourImage.Width;
+                int sourHeight = sourImage.Height;
+                if (sourHeight > destHeight || sourWidth > destWidth)
+                {
+                    if ((sourWidth * destHeight) > (sourHeight * destWidth))
+                    {
+                        width = destWidth;
+                        height = (destWidth * sourHeight) / sourWidth;
+                    }
+                    else
+                    {
+                        height = destHeight;
+                        width = (sourWidth * destHeight) / sourHeight;
+                    }
+                }
+                else
+                {
+                    width = sourWidth;
+                    height = sourHeight;
+                }
+                Bitmap destBitmap = new Bitmap(destWidth, destHeight);
+                Graphics g = Graphics.FromImage(destBitmap);
+                g.Clear(Color.Transparent);
+                //设置画布的描绘质量           
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(sourImage, new Rectangle((destWidth - width) / 2, (destHeight - height) / 2, width, height), 0, 0, sourImage.Width, sourImage.Height, GraphicsUnit.Pixel);
+                //g.DrawImage(sourImage, new Rectangle(0, 0, destWidth, destHeight), new Rectangle(0, 0, sourImage.Width, sourImage.Height), GraphicsUnit.Pixel);
+                g.Dispose();
+                //设置压缩质量       
+                System.Drawing.Imaging.EncoderParameters encoderParams = new System.Drawing.Imaging.EncoderParameters();
+                long[] quality = new long[1];
+                quality[0] = 100;
+                System.Drawing.Imaging.EncoderParameter encoderParam = new System.Drawing.Imaging.EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+                encoderParams.Param[0] = encoderParam;
+                sourImage.Dispose();
+                return destBitmap;
+            }
+            catch (Exception ex)
+            {
+                return bitmap;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PDF文件|*.pdf";
+            ofd.FileName = string.Empty;
+            ofd.Title = "选择PDF文件";
+            if (ofd.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(ofd.FileName))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPG文件|*.jpg";
+                saveFileDialog.Title = "保存图像";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(saveFileDialog.FileName))
+                {
+                    try
+                    {
+                        ConvertPdfToImage(ofd.FileName, saveFileDialog.FileName, double.Parse(txtBoxImageSize.Text));
+                        MessageBox.Show("转换完成");
+                        System.Diagnostics.Process.Start("Explorer", "/select," + saveFileDialog.FileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+        }
+    }
+}
